f23b4c6add2551081154f6751000e922cf498625
sivakumarkatam <siva.katam@blazeclan.com>
2017-04-12 17:23:51 +0530

    Update startApp.bash

--- Scripts/startApp.bash
+++ Scripts/startApp.bash
@@ -1,2 +1,2 @@
-chmod +x /var/tmp/spring-boot-webapp-0.0.1-SNAPSHOT.war
+sudo chmod +x /var/tmp/spring-boot-webapp-0.0.1-SNAPSHOT.war
 java -jar /var/tmp/spring-boot-webapp-0.0.1-SNAPSHOT.war > /dev/null 2> /dev/null < /dev/null &
