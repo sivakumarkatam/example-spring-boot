12bfa15947f7f4db4b7b85b8af7d8a966322b216
sivakumarkatam <siva.katam@blazeclan.com>
2017-04-21 09:46:45 +0530

    Update serviceApp.bash

--- Scripts/serviceApp.bash
+++ Scripts/serviceApp.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp -f /var/lib/jenkins/workspace/java/target/spring-boot-webapp-0.0.1-SNAPSHOT.war /tmp/
 cp -f /tmp/spring-boot-webapp-0.0.1-SNAPSHOT.war /var/tmp/spring-boot-webapp-0.0.1-SNAPSHOT.war
 
 sudo ln -fs /var/tmp/spring-boot-webapp-0.0.1-SNAPSHOT.war /etc/init.d/spring-boot-new
