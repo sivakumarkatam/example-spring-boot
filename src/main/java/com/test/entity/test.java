daf752079c402fb00f5cb29ae4ef532bf5ddab3f
sivakumarkatam <siva.katam@blazeclan.com>
2017-11-27 18:34:30 +0530

    Update test.java

--- src/main/java/com/test/entity/test.java
+++ src/main/java/com/test/entity/test.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
+    private String AWSPassword="siuva.jhiuu";
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-        private String AWSPassword="siuva.jhiuu";
         System.exit( 0 ); //success
     }
 }
