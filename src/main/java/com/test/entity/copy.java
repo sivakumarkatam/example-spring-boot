d35960a0bee484af20510746c66f9136ee0317ba
sivakumarkatam <siva.katam@blazeclan.com>
2017-11-28 11:26:53 +0530

    Create copy.java

---
+++ src/main/java/com/test/entity/copy.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    private String AWSPassword="siuva.jhiuu";
+    private String AWS_AccessKey="siuva.jhiuu";
+    private String AWSsecretkey="siuva.jhiuu";
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+         System.out.println( "" + AWSPassword );
+        System.exit( 0 ); //success
+    }
+}
