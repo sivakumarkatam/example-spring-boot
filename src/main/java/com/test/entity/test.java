0befc1be842d19a3f7132586d19cf59402c7d6eb
sivakumarkatam <siva.katam@blazeclan.com>
2017-11-28 11:25:24 +0530

    Update test.java

--- src/main/java/com/test/entity/test.java
+++ src/main/java/com/test/entity/test.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     private String AWSsecretkey="siuva.jhiuu";
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+         System.out.println( "" + AWSPassword );
         System.exit( 0 ); //success
     }
 }
