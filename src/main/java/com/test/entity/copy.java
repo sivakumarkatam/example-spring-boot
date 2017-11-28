281b38891b3f95106343b5b6bb0ec32c53040af7
sivakumarkatam <siva.katam@blazeclan.com>
2017-11-28 11:31:38 +0530

    Update copy.java

--- src/main/java/com/test/entity/copy.java
+++ src/main/java/com/test/entity/copy.java
@@ -8,3 +8,14 @@ public class HelloWorld {
         System.exit( 0 ); //success
     }
 }
+
+public class HelloWorld {
+    private String AWSPassword="siuva.jhiuu";
+    private String AWS_AccessKey="siuva.jhiuu";
+    private String AWSsecretkey="siuva.jhiuu";
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+         System.out.println( "" + AWSPassword );
+        System.exit( 0 ); //success
+    }
+}
